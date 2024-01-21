87cc323c210597d08f829b97d78e6c9825d82598
AdrienETS <adrien.josephine-olivier.1@ens.etsmtl.ca>
2024-01-21 04:57:35 -0500

    Formatter et stocker les réponses utilisateurs

--- src/components/ChatBot/BotMessage.js
+++ src/components/ChatBot/BotMessage.js
@@ -10,15 +10,15 @@ export default function BotMessage({ message, onOptionSelected }) {
     <div className="message-container">
       {message.type === 'question' ? (
         <div>
-          <div className="bot-message">{message.text}</div>
+          <div className="bot-message">{message.question}</div>
           <div className="bot-options">
-            {message.options.map((option, index) => (
-              <button key={index} onClick={() => onOptionSelected(option, index)}>{option}</button>
+            {message.reponseText && message.reponseText.map((option, index) => (
+              <button key={index} onClick={() => onOptionSelected(option, index, message.id)}>{option}</button>
             ))}
           </div>
         </div>
       ) : (
-        <div className="bot-message">{message.text}</div>
+        <div className="bot-message">{message.question}</div>
       )}
     </div>
   );
--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -11,113 +11,118 @@ import API from "./ChatBotAPI";
 import "./styles.css";
 
 function Chatbot() {
-  const [messages, setMessages] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [messages, setMessages] = useState([]);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [userResponses, setUserResponses] = useState([]);
 
-  const questions = [
-    {
-      text: "What's your favorite color?",
-      options: ['Red', 'Blue', 'Green'],
-    },
-    {
-      text: "What's your favorite animal?",
-      options: ['Dog', 'Cat', 'Bird'],
-    },
-    {
-        text: "What's your favorite month?",
-        options: ['Jan', 'Feb', 'mar'],
-      },
-    // Add more questions as needed
-  ];
+    const questions = [
+        {id: 'Autre1', question: 'Est vous un nouveau entrepreneur?', reponseText: [ "non", "oui"], reponseValue: [ 0, 1]},
+        {id: 'Autre2', question: 'Avez vous fait un business case?', reponseText: [ "non", "oui"], reponseValue: [ 0, 1]},
+        {id: 'Autre3', question: 'Avec vous un business plan?', reponseText: [ "non", "oui"], reponseValue: [ 0, 1]},
+        {id: 'Autre4', question: 'Avez vous fait une analyse sur le retour d investissement?', reponseText: [ "non", "oui"], reponseValue: [ 0, 1]},
+    ];
 
-  useEffect(() => {
-    async function loadWelcomeMessage() {
-      const welcomeMessage = {
-        text: "Welcome to the chatbot! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere ornare sapien, a imperdiet tellus iaculis vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi accumsan, purus non dignissim eleifend, felis sem varius diam, nec imperdiet urna nunc malesuada ligula. Nullam ornare bibendum diam. Click 'Start' to begin.",
-        options: ['Start'],
-        type: 'question'
-      };
-  
-      setMessages([
-        <BotMessage
-          key="0"
-          message={welcomeMessage}
-          onOptionSelected={handleOptionSelected}
-        />
-      ]);
-    }
-    loadWelcomeMessage();
-  }, []);
-
-  const handleOptionSelected = (option, index) => {
-    if (option === 'Start') {
-      // Load the first question when 'Start' is selected
-      const firstQuestion = questions[currentQuestionIndex];
-      const botMessage = (
-        <BotMessage
-          key="1"
-          message={{ ...firstQuestion, type: 'question' }}
-          onOptionSelected={handleOptionSelected}
-        />
-      );
-      setMessages(prevMessages => [...prevMessages, botMessage]);
-    } else {
-      // Handle other options as before
-      send(option, true, index);
-    }
-  };
-
-  const send = async (text, isOption = false, optionIndex = null) => {
-    let displayText = text;
-    if (isOption && optionIndex !== null) {
-        // If it's an option, append the index to the display text
-        //displayText = `${text} (option ${optionIndex + 1})`;
-    }
     
-    // Add the user's message
-    const userMessage = <UserMessage key={messages.length + 1} text={displayText} />;
-    setMessages(prevMessages => [...prevMessages, userMessage]);
+    useEffect(() => {
+        {/*  When userResponses changes, do ...*/}
+    }, [userResponses]);
+
+    useEffect(() => {
+        async function loadWelcomeMessage() {
+            const welcomeMessage = {
+                id: 0,
+                question: "Welcome to the chatbot! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere ornare sapien, a imperdiet tellus iaculis vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi accumsan, purus non dignissim eleifend, felis sem varius diam, nec imperdiet urna nunc malesuada ligula. Nullam ornare bibendum diam. Click 'Start' to begin.",
+                reponseText: ['Start'],
+                reponseValue: [0],
+                type: 'question'
+            };
   
-    if (isOption) {
-      // Increment currentQuestionIndex to load the next question
-      setCurrentQuestionIndex(currentIndex => {
-        const nextIndex = currentIndex + 1;
-        if (nextIndex < questions.length) {
-          const nextQuestion = questions[nextIndex];
-          const botMessage = (
-            <BotMessage
-              key={messages.length + 2}
-              message={{ ...nextQuestion, type: 'question' }}
-              onOptionSelected={handleOptionSelected}
-            />
-          );
-          setMessages(prevMessages => [...prevMessages, botMessage]);
+            setMessages([
+                <BotMessage
+                key="0"
+                message={welcomeMessage}
+                onOptionSelected={handleOptionSelected}
+                />
+            ]);
         }
-        return nextIndex;
-      });
-    } else {
-      // Handle regular text message (e.g., initial greeting or non-option responses)
-      const botResponse = await API.GetChatbotResponse(text);
-      if (botResponse) {
+        loadWelcomeMessage();
+    }, []);
+
+    const handleOptionSelected = (option, optionIndex, messageId) => {
+        if (option === 'Start') {
+
+            loadFirstQuestion();
+
+        } else {
+
+            // Store user response
+
+            const currentQuestion = questions[currentQuestionIndex];
+            const response = {
+            questionId: messageId,
+            optionId: optionIndex,
+            option: option,
+            reponseValue: currentQuestion.reponseValue[optionIndex]
+            };
+
+            setUserResponses(prevResponses => [...prevResponses, response]);
+
+            // Handle other options as before
+            send(option, true, optionIndex);
+
+            loadNextQuestion();
+        }
+    };
+
+    const loadFirstQuestion = () => {
+        // Load the first question when 'Start' is selected
+        const firstQuestion = questions[currentQuestionIndex];
         const botMessage = (
-          <BotMessage
-            key={messages.length + 2}
-            message={botResponse}
+        <BotMessage
+            key="1"
+            message={{ ...firstQuestion, type: 'question' }}
             onOptionSelected={handleOptionSelected}
-          />
+        />
         );
         setMessages(prevMessages => [...prevMessages, botMessage]);
-      }
     }
-  };
 
-  return (
-    <div className="chatbot">
-        <Header />
-        <Messages messages={messages} />
-        <Input onSend={send} />
-    </div>
-  );
+    const loadNextQuestion = () => {
+        // Increment currentQuestionIndex to load the next question
+        setCurrentQuestionIndex(currentIndex => {
+        const nextIndex = currentIndex + 1;
+        if (nextIndex < questions.length) {
+            sendQuestion(questions[nextIndex]);
+        }
+        return nextIndex;
+        });
+    };
+  
+    const sendQuestion = (question) => {
+        const botMessage = (
+        <BotMessage
+            key={messages.length + 1}
+            message={{ ...question, type: 'question' }}
+            onOptionSelected={handleOptionSelected}
+        />
+        );
+        setMessages(prevMessages => [...prevMessages, botMessage]);
+    };
+
+
+    const send = async (text, isOption = false, optionIndex = null) => {
+        // Add the user's message
+        const userMessage = <UserMessage key={messages.length + 1} text={text} />;
+        setMessages(prevMessages => [...prevMessages, userMessage]);
+    };
+
+    return (
+        <div className="chatbot">
+            <Header />
+            <Messages messages={messages} />
+            <Input onSend={send} />
+        </div>
+    );
 }
 
 export default Chatbot;
